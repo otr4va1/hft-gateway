9011e2bed48fe4c43f8bdd663e1e9dd28f20db38
fsociety <leffbrut@gmail.com>
2026-09-09 17:43:01 +0500

    impl reading websocket

--- hft-gateway/src/main.rs
+++ hft-gateway/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use tokio_tungstenite::{connect_async,tungstenite::error::Error};
+use futures_util::{StreamExt,};
+#[tokio::main]
+async fn main() -> Result<(),Error> {
+    let url = "wss://stream.binance.com:9443/ws/btcusdt@trade";
+    let (ws_stream, _) = connect_async(url).await?;
+
+    let (_ws_writer, mut ws_reader) = ws_stream.split();
+    while let Some(message_result) = ws_reader.next().await {
+        match message_result {
+            Ok(message) => {
+                if message.is_text() {
+                    println!("Received message: {}", message.to_text()?);
+                }
+            }
+            Err(e) => {
+                println!("Error receiving message: {}", e);
+            }
+        }
+    }
+    Ok(())
 }
