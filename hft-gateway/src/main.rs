9420206512c41578f682d25196dd846536a16eec
leff brut <leffbrut@gmail.com>
2026-09-13 11:54:33 +0500

    impl autoreconnect

--- hft-gateway/src/main.rs
+++ hft-gateway/src/main.rs
@@ -1,22 +1,64 @@
-use tokio_tungstenite::{connect_async,tungstenite::error::Error};
-use futures_util::{StreamExt,};
+use tokio::time::Duration;
+use futures_util::{StreamExt, SinkExt};
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{
+        error::Error,
+        Message::{Binary, Close, Frame, Ping, Pong, Text},
+    }
+};   
+
+const RECONECT_AFTER: Duration = Duration::from_secs(5);
+
+
 #[tokio::main]
 async fn main() -> Result<(),Error> {
     let url = "wss://stream.binance.com:9443/ws/btcusdt@trade";
-    let (ws_stream, _) = connect_async(url).await?;
+    
+    loop {
+        println!("Connecting to {}", url);
+        
+        let (ws_stream, _) = match connect_async(url).await {
+            Ok(s) => {
+                println!("Succecfully connected!");
+                s
+            },
+            Err(e) => {
+                println!("Connection error: {}, Retring in {:?}...", e, RECONECT_AFTER);
+                tokio::time::sleep(RECONECT_AFTER).await;
+                continue;
+            }
+        };
+        let (mut ws_writer, mut ws_reader) = ws_stream.split();
 
-    let (_ws_writer, mut ws_reader) = ws_stream.split();
-    while let Some(message_result) = ws_reader.next().await {
-        match message_result {
-            Ok(message) => {
-                if message.is_text() {
-                    println!("Received message: {}", message.to_text()?);
+        while let Some(message_result) = ws_reader.next().await {
+                match message_result {
+                    Ok(message) => match message {
+                        Text(trade) => {
+                            println!("Received trade: {}", trade.as_str());
+                        },
+                        Ping(playload) => {
+                            println!("Recieved Ping. Replying with Pong...");
+                            if let Err(e) = ws_writer.send(Pong(playload)).await {
+                                println!("Failed to pong server: {}. Reconnecting...", e);
+                                break;
+                            }
+                        },
+                        Pong(_) => {},
+                        Binary(_) => {},
+                        Close(_) => {
+                            println!("Server closed connection. Reconnecting...");
+                            break;
+                        },
+                        Frame(_) => {}
+                    }
+                
+                    Err(e) => {
+                        println!("Error receiving message: {}. Reconnecting", e);
+                        break;
+                    }
                 }
             }
-            Err(e) => {
-                println!("Error receiving message: {}", e);
-            }
-        }
+        tokio::time::sleep(RECONECT_AFTER).await;
     }
-    Ok(())
 }
